e04b1aa25fd780f5bf5e728d68a65588f092af5a
ParthDwivedi1 <73470152+ParthDwivedi1@users.noreply.github.com>
2021-09-04 17:47:39 +0530

    Added Cupertino Style Widget and LME/MCX price

--- lib/models/stock.dart
+++ lib/models/stock.dart
@@ -1,48 +1,54 @@
-
 class Stock {
+  String symbol = "";
+  String metal = "";
+  double priceShaizi = 0;
+  double priceCc = 0;
+  double priceLME = 0;
+  double priceMCX = 0;
+  double priceDisplayLocal = 0;
+  double priceDisplayMain = 0;
 
-  String symbol="";
-  String metal="";
-  double priceShaizi=0;
-  double priceCc=0;
-  double priceLME=0;
-  double priceDisplay=0;
-  int co=0;
-
-  Stock(String symbol, String metal, double priceShaizi,double priceCc, double priceLME){
-    this.symbol=symbol;
-    this.metal=metal;
-    this.priceShaizi=priceShaizi;
-    this.priceDisplay=priceShaizi;
-    this.priceCc=priceCc;
-    this.priceLME=priceLME;
-    this.co=0;
+  Stock(String symbol, String metal, double priceShaizi, double priceCc,
+      double priceLME, double priceMCX) {
+    this.symbol = symbol;
+    this.metal = metal;
+    this.priceShaizi = priceShaizi;
+    this.priceDisplayLocal = priceShaizi;
+    this.priceCc = priceCc;
+    this.priceLME = priceLME;
+    this.priceMCX = priceMCX;
+    this.priceDisplayMain = priceLME;
   }
 
   static List<Stock> getAll() {
-
     List<Stock> stocks = <Stock>[];
-    stocks.add(Stock("Lead", "Pb", 258, 100,50));
-    stocks.add(Stock("Copper","Cu", 800, 200,50));
-    stocks.add(Stock( "General Electronic", "GE", 56.00,10,50));
-    stocks.add(Stock( "Home Depot", "HE",  10, 178,50));
-    stocks.add(Stock("Evergreen Solar", "EVR",10,  9.0,50));
-    stocks.add(Stock("Facebook", "FB", 10, 200,50));
-    stocks.add(Stock("Samsung", "SAM", 10,  134,50));
-    stocks.add(Stock("Snapchat", "SNAP", 10, 45,50));
-    stocks.add(Stock("Microsoft", "MSOFT",10, 400,50));
-    stocks.add(Stock("Google", "GOOG",10, 1800,50));
+    stocks.add(Stock("Lead", "Pb", 258, 100, 50, 100));
+    stocks.add(Stock("Copper", "Cu", 800, 200, 50, 10000));
+    stocks.add(Stock("General Electronic", "GE", 56.00, 10, 50, 10000));
+    stocks.add(Stock("Home Depot", "HE", 10, 178, 50, 10000));
+    stocks.add(Stock("Evergreen Solar", "EVR", 10, 9.0, 50, 10000));
+    stocks.add(Stock("Facebook", "FB", 10, 200, 50, 10000));
+    stocks.add(Stock("Samsung", "SAM", 10, 134, 50, 10000));
+    stocks.add(Stock("Snapchat", "SNAP", 10, 45, 50, 10000));
+    stocks.add(Stock("Microsoft", "MSOFT", 10, 400, 50, 10000));
+    stocks.add(Stock("Google", "GOOG", 10, 1800, 50, 10000));
 
     return stocks;
-
   }
 
-  void priceChange(int co){
-    if(co==1){
-      priceDisplay=priceCc;
-    }else{
-      priceDisplay=priceShaizi;
+  void priceChangeLocal(int? co) {
+    if (co == 1) {
+      priceDisplayLocal = priceCc;
+    } else {
+      priceDisplayLocal = priceShaizi;
     }
   }
 
+  void priceChangeMain(int? co) {
+    if (co == 1) {
+      priceDisplayMain = priceMCX;
+    } else {
+      priceDisplayMain = priceLME;
+    }
+  }
 }
--- lib/widgets/stockList.dart
+++ lib/widgets/stockList.dart
@@ -1,4 +1,4 @@
-
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:stockclone/models/stock.dart';
 
@@ -6,11 +6,13 @@ class StockList extends StatefulWidget {
   @override
   _StockListState createState() => _StockListState();
 }
-class _StockListState extends State<StockList>{
 
-  List<Stock> stocks=Stock.getAll();
-  String stockName="";
-  List<bool> isSelected=<bool>[true,false];
+class _StockListState extends State<StockList> {
+  List<Stock> stocks = Stock.getAll();
+  String stockName = "";
+  List<bool> isSelected = <bool>[true, false];
+  int? controlLocal = 0;
+  int? controlMain = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -20,23 +22,24 @@ class _StockListState extends State<StockList>{
           children: <Widget>[
             Expanded(
               flex: 7,
-              child: Text("Metal",
+              child: Text(
+                "Metal",
                 style: TextStyle(
                     color: Colors.grey[500],
                     fontSize: 30,
-                    fontWeight: FontWeight.bold
-                ),
+                    fontWeight: FontWeight.bold),
               ),
             ),
             Expanded(
-              flex: 4,
+              flex: 3,
               child: SizedBox(),
             ),
             Expanded(
               flex: 8,
-              child: ToggleButtons(
-                children: <Widget>[
-                  Text("Shaizi",
+              child: CupertinoSlidingSegmentedControl(
+                children: <int, Widget>{
+                  0: Text(
+                    "Shaizi",
                     textAlign: TextAlign.center,
                     style: TextStyle(
                       color: Colors.grey[500],
@@ -44,7 +47,8 @@ class _StockListState extends State<StockList>{
                       fontWeight: FontWeight.bold,
                     ),
                   ),
-                  Text("CC",
+                  1: Text(
+                    "CC",
                     textAlign: TextAlign.center,
                     style: TextStyle(
                       color: Colors.grey[500],
@@ -52,45 +56,63 @@ class _StockListState extends State<StockList>{
                       fontWeight: FontWeight.bold,
                     ),
                   ),
-                ],
-                onPressed: (int index) {
+                },
+                onValueChanged: (int? i) {
                   setState(() {
+                    controlLocal = i;
                     for (Stock stock in stocks) {
-                      stock.priceChange(index);
-                    }
-                    for (int buttonIndex = 0; buttonIndex <
-                        isSelected.length; buttonIndex++) {
-                      if (buttonIndex == index) {
-                        isSelected[buttonIndex] = true;
-                      } else {
-                        isSelected[buttonIndex] = false;
-                      }
+                      stock.priceChangeLocal(controlLocal);
                     }
                   });
                 },
-                isSelected: isSelected,
-                fillColor: Colors.grey[900],
-                borderRadius: BorderRadius.circular(10),
+                groupValue: controlLocal,
+                //isSelected: isSelected,
+                //fillColor: Colors.grey[900],
+                //borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+            Expanded(flex: 2, child: SizedBox(width: 30)),
+            Expanded(
+              flex: 7,
+              child: CupertinoSlidingSegmentedControl(
+                children: <int, Widget>{
+                  0: Text(
+                    "LME",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.grey[500],
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  1: Text(
+                    "MCX",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.grey[500],
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                },
+                onValueChanged: (int? i) {
+                  setState(() {
+                    controlMain = i;
+                    for (Stock stock in stocks) {
+                      stock.priceChangeMain(controlMain);
+                    }
+                  });
+                },
+                groupValue: controlMain,
+                //isSelected: isSelected,
+                //fillColor: Colors.grey[900],
+                //borderRadius: BorderRadius.circular(10),
               ),
             ),
             Expanded(
-                flex:2,
-                child: SizedBox(width:30)),
-            Expanded(
-              flex: 5,
-              child: Text("LME",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    color: Colors.grey[500],
-                    fontSize: 15,
-                    fontWeight: FontWeight.bold
-                ),
-              ),
-            ),
-            Expanded(
-                flex:4,
-                child:SizedBox(),
-            ),
+              flex: 2,
+              child: SizedBox(),
+            )
           ],
         ),
         SizedBox(
@@ -98,12 +120,12 @@ class _StockListState extends State<StockList>{
         ),
         Row(
           children: <Widget>[
-            Text("Last Update",
+            Text(
+              "Last Update",
               style: TextStyle(
                   color: Colors.grey[500],
                   fontSize: 18,
-                  fontWeight: FontWeight.bold
-              ),
+                  fontWeight: FontWeight.bold),
             ),
           ],
         ),
@@ -111,96 +133,113 @@ class _StockListState extends State<StockList>{
           height: 15.0,
         ),
         Expanded(
-          flex:1,
+          flex: 1,
           child: ListView.separated(
               scrollDirection: Axis.vertical,
-              shrinkWrap:true,
-              physics: BouncingScrollPhysics(parent:AlwaysScrollableScrollPhysics()),
+              shrinkWrap: true,
+              physics: BouncingScrollPhysics(
+                  parent: AlwaysScrollableScrollPhysics()),
               separatorBuilder: (context, index) {
-              return Divider(color: Colors.grey[400]);
-            },
-            itemCount: this.stocks.length,
-            itemBuilder: (context, index) {
+                return Divider(color: Colors.grey[400]);
+              },
+              itemCount: this.stocks.length,
+              itemBuilder: (context, index) {
+                Stock stock = this.stocks[index];
 
-              Stock stock = this.stocks[index];
-
-              return ExpansionTile(
-                children: <Widget>[
-                  Column(
-                    children: _buildExpandableContent(stock,context,stockName),
-                  )
-                ],
-                title: Row(
-                  children: <Widget>[
-                    Expanded(
-                      flex:8,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text("${stock.symbol}", style: TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.w500)),
-                          Text("${stock.metal}",style: TextStyle(color: Colors.grey[500], fontSize: 20))
-                        ],
-                      ),
-                    ),
-                    Expanded(
-                      flex:1,
-                      child: SizedBox(),
-                    ),
-                    Expanded(
-                      flex:6,
-                      child: Column(
-                        children: <Widget>[
-                          Text("\$${stock.priceDisplay}", style: TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.w500)),
-                          Container(
-                              alignment: Alignment.center,
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(5),
-                                  color: Colors.red
-                              ),
-                              width: 75,
-                              height: 25,
-                              child: Text("-1.09%",style: TextStyle(color: Colors.white))
-                          )
-                        ],
-                      ),
-                    ),
-                    Expanded(
-                      flex:2,
-                      child: SizedBox(),
-                    ),
-                    Expanded(
-                      flex:5,
-                      child: Column(
-                        children: <Widget>[
-                          Text("\$${stock.priceLME}", style: TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.w500)),
-                          Container(
-                              alignment: Alignment.center,
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(5),
-                                  color: Colors.red
-                              ),
-                              width: 75,
-                              height: 25,
-                              child: Text("-1.09%",style: TextStyle(color: Colors.white))
-                            )
-                          ],
-                        ),
-                    ),
+                return ListTileTheme(
+                  contentPadding: EdgeInsets.all(0),
+                  child: ExpansionTile(
+                    children: <Widget>[
+                      Column(
+                        children:
+                            _buildExpandableContent(stock, context, stockName),
+                      )
                     ],
+                    title: Row(
+                      children: <Widget>[
+                        Expanded(
+                          flex: 8,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(
+                                "${stock.symbol}",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.w500),
+                              ),
+                              Text("${stock.metal}",
+                                  style: TextStyle(
+                                      color: Colors.grey[500], fontSize: 20))
+                            ],
+                          ),
+                        ),
+                        Expanded(
+                          flex: 1,
+                          child: SizedBox(),
+                        ),
+                        Expanded(
+                          flex: 6,
+                          child: Column(
+                            children: <Widget>[
+                              Text("\$${stock.priceDisplayLocal}",
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 24,
+                                      fontWeight: FontWeight.w500)),
+                              Container(
+                                  alignment: Alignment.center,
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(5),
+                                      color: Colors.red),
+                                  width: 75,
+                                  height: 25,
+                                  child: Text("-1.09%",
+                                      style: TextStyle(color: Colors.white)))
+                            ],
+                          ),
+                        ),
+                        Expanded(
+                          flex: 2,
+                          child: SizedBox(),
+                        ),
+                        Expanded(
+                          flex: 5,
+                          child: Column(
+                            children: <Widget>[
+                              Text("\$${stock.priceDisplayMain}",
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 24,
+                                      fontWeight: FontWeight.w500)),
+                              Container(
+                                  alignment: Alignment.center,
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(5),
+                                      color: Colors.red),
+                                  width: 75,
+                                  height: 25,
+                                  child: Text("-1.09%",
+                                      style: TextStyle(color: Colors.white)))
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 );
-            }
-          ),
+              }),
         ),
-    ],
+      ],
     );
   }
-  _buildExpandableContent(Stock stock, BuildContext context, String stockName)
-  {
-    List<Widget> widgets= <Widget>[];
+
+  _buildExpandableContent(Stock stock, BuildContext context, String stockName) {
+    List<Widget> widgets = <Widget>[];
     widgets.add(
       GestureDetector(
-        onTap:(){
+        onTap: () {
           stockName = stock.metal;
           Scaffold.of(context).openEndDrawer();
         },
@@ -211,14 +250,14 @@ class _StockListState extends State<StockList>{
             color: Colors.grey[800],
             borderRadius: BorderRadius.all((Radius.circular(20.0))),
           ),
-          child: Text(
-              stock.metal,
-              style:TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.w500)
-          ),
+          child: Text(stock.metal,
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w500)),
         ),
       ),
     );
     return widgets;
-
   }
 }
